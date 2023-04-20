fa91e9bbd4802a61b4bd0b471f41b594b8648e64
alv6s <alvescvlp@gmail.com>
2023-04-20 19:53:04 +0100

    213

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pevieira <pevieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 11:54:40 by pevieira          #+#    #+#             */
-/*   Updated: 2023/04/20 19:44:34 by pevieira         ###   ########.fr       */
+/*   Updated: 2023/04/20 19:52:21 by pevieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	i = 0;
 	if (*little == '\0')
 		return ((char *)big);
-	if (big == '\0' && len == 0)
-		return (0);
+	if (big == NULL && len == 0)
+		return (NULL);
 	while (big[i] != '\0' && i < len)
 	{
 		j = 0;
 		while (big[i + j] == little[j])
 		{
-			if (little[j + 1] == '\0')
+			if (little[j + 1] == '\0' && i + j < len)
 				return ((char *)&big[i]);
 			j++;
 		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
