d052e3dbcd1bb9a386afccbb6ad3f3796a3b856c
alv6s <alvescvlp@gmail.com>
2023-05-19 13:09:31 +0100

    mainprintf

--- Printf/main.c
+++ Printf/main.c
@@ -6,7 +6,7 @@
 /*   By: pevieira <pevieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 15:28:10 by pevieira          #+#    #+#             */
-/*   Updated: 2023/05/19 11:21:59 by pevieira         ###   ########.fr       */
+/*   Updated: 2023/05/19 13:09:02 by pevieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	main(void)
 
 	x1 = 'A';
 	x2 = "Ola tudo bem?";
-	ft_printf("Caracter: %c, String: %s", x1, x2);
+	ft_printf("O meu teste\n ____________\nCaracter: %c\nString: %s\nPointer: %p\nDecimal: %d\nInt: %i\nUnsigned Decimal: %u\nNbr hexadecimal: %x\nNBR HEXADECIMAL: %X\nicone: %%\n\n", x1, x2, &x2, 132, 0, 55, 42, 42);
+	printf("O printf original\n ____________\nCaracter: %c\nString: %s\nPointer: %p\nDecimal: %d\nInt: %i\nUnsigned Decimal: %u\nNbr hexadecimal: %x\nNBR HEXADECIMAL: %X\nicone: %%\n", x1, x2, &x2, 132, 0, 55, 42, 42);
 	return (0);
 }
