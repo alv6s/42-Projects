878b6ed82ce511c20e7a9fe6fd2deb6dbad12fcd
alv6s <alvescvlp@gmail.com>
2023-11-04 19:10:13 +0000

    gits

--- So_long/src/map_render.c
+++ So_long/src/map_render.c
@@ -8,3 +8,7 @@ https://reactive.so/post/42-a-comprehensive-guide-to-so_long
 https://harm-smits.github.io/42docs/libs/minilibx/hooks.html
 
 https://cdn.intra.42.fr/pdf/pdf/105166/en.subject.pdf
+
+https://github.com/LuisBalsa/so_long
+
+https://github.com/someoneisanna/42-so_long
